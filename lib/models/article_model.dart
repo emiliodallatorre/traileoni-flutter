947f0843f320ab466ff3e27a41712cc7e559231c
Emilio Dalla Torre <emiliodallatorre12@live.com>
2020-08-30 11:57:29 +0200

    Effettuati alcuni regex per il tempo di lettura e i "..." alla fine del testo.
    Took 20 minutes

--- lib/models/article_model.dart
+++ lib/models/article_model.dart
@@ -26,10 +26,10 @@ class ArticleModel {
 
   factory ArticleModel.fromWordpressPost(Post wordpressPost) => ArticleModel(
         title: parse(wordpressPost.title.rendered).documentElement.text,
-        excerpt: parse(wordpressPost.excerpt.rendered).documentElement.text,
+        excerpt: parse(wordpressPost.excerpt.rendered).documentElement.text.replaceFirst("Reading time:", "[\u{23F3}:").replaceFirst(" minutes", " minutes]").replaceFirst(" \u{2026} Continue", "\u{2026}"),
         featuredMediaUrl: wordpressPost.featuredMedia != null ? wordpressPost.featuredMedia.link : null,
         featuredMediaCaption: wordpressPost.featuredMedia != null ? wordpressPost.featuredMedia.title.rendered : null,
-        htmlBody: wordpressPost.content.rendered,
+        htmlBody: wordpressPost.content.rendered.replaceFirst("Reading time:", "\u{23F3} Reading time:"),
         plainBody: parse(wordpressPost.content.rendered).documentElement.text.replaceAll("\n\n", "\n").trim(),
         categories:
             wordpressPost.categories != null ? wordpressPost.categories.map((rawCategory) => CategoryModel.fromWordpressCategory(rawCategory)).toList() : null,
